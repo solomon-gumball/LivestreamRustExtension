f337eeba36bea52eddce6c0bcaa553421fe46262
Joseph Sample <joseph.michael.sample@gmail.com>
2026-04-08 04:41:25 -0700

    importing more datatypes, now successfully subscribing and getting chatter data

--- godot/start_scene.gd
+++ godot/start_scene.gd
@@ -4,8 +4,14 @@ extends Node2D
 
 func _ready() -> void:
   network_handler.emote_triggered.connect(_handle_emote_triggered)
+  network_handler.chatter_updated.connect(_handle_chatter_updated)
+
   # network_handler.scrolling_text_updated.connect(
 
+func _handle_chatter_updated(chatter: Chatter) -> void:
+  print(chatter)
+
 func _handle_emote_triggered(chatter: Chatter, emote: String) -> void:
+  print('chatter ', chatter)
   return
   
