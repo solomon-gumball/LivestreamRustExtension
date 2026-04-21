091e81ec9dfa6fd7ea869bb6a3c28f69c8e7d502
solomon-gumball <joseph.michael.sample@gmail.com>
2026-04-21 14:25:51 -0700

    state machine

---
+++ godot/stream_overlay/roaming_bots.gd
@@ -0,0 +1,52 @@
+extends Node3D
+class_name RoamingBots
+
+@onready var camera: Camera3D = %Camera
+@onready var bot_spawn_location: Node3D = %BotSpawnLocation
+
+var spawned_bots = {}
+
+func _ready():
+  WSClient.authenticated_state.chat_message_received.connect(chat_message_received)
+  WSClient.authenticated_state.store_data_received.connect(store_data_received)
+  WSClient.authenticated_state.chatter_updated.connect(chatter_updated)
+  WSClient.authenticated_state.emote_triggered.connect(emote_triggered)
+
+  store_data_received()
+
+func emote_triggered(chatter: Chatter, emote: String):
+  var bot := get_or_create_bot_for_user(chatter)
+  bot.emote = emote
+
+func chatter_updated(updated: Chatter):
+  var bot := get_or_create_bot_for_user(updated)
+  bot.chatter = updated
+
+func store_data_received():
+  for chatter in WSClient.authenticated_state.active_chatters:
+    get_or_create_bot_for_user(chatter)
+
+const gumbot_template: PackedScene = preload("res://stream_overlay/stream_overlay_gumbot.tscn")
+const DEFAULT_BOT_SCALE = Vector3(0.6, 0.6, 0.6)
+func get_or_create_bot_for_user(chatter: Chatter) -> GumBot:
+  if spawned_bots.has(chatter.id):
+    return spawned_bots[chatter.id]
+  else:
+    var bot: GumBot = gumbot_template.instantiate()
+    spawned_bots[chatter.id] = bot
+
+    add_child(bot)
+    bot.chatter = chatter
+
+    bot.global_position = bot_spawn_location.global_position + Vector3(randf_range(-1.5, 1.5), 0, randf_range(-.5, .5))
+    return bot
+
+func bot_expired(bot: GumBot):
+  bot.queue_free()
+  spawned_bots.erase(bot.chatter.id)
+
+func chat_message_received(message: Message.Chat):
+  var bot := get_or_create_bot_for_user(message.chatter)
+  bot.chatter = message.chatter
+
+  print("Chat message received: ", message.message, " from ", message.chatter.display_name)
--- godot/stream_overlay/stream_overlay.gd
+++ godot/stream_overlay/stream_overlay.gd
@@ -1,64 +1,24 @@
 extends Node3D
 class_name OverlayScene
 
-@onready var camera: Camera3D = %Camera
-@onready var bot_spawn_location: Node3D = %BotSpawnLocation
-@onready var game_root_node: Node3D = %GameRootNode
-@onready var bot_walking_scene: Node3D = %BotWalkingScene
-
-var spawned_bots = {}
+var state = StateMachine.new()
+var roaming_state = RoamingState.new(self)
+var game_state = GameState.new(self)
 
 func _ready():
-  WSClient.authenticated_state.chat_message_received.connect(chat_message_received)
-  WSClient.authenticated_state.store_data_received.connect(store_data_received)
-  WSClient.authenticated_state.chatter_updated.connect(chatter_updated)
-  WSClient.authenticated_state.emote_triggered.connect(emote_triggered)
   MultiplayerClient.current_lobby_updated.connect(_handle_lobby_updated)
 
-  store_data_received()
   MultiplayerClient.start()
-  # WSClient.state.changed.connect(_handle_ws_state_changed)
+  WSClient.state.changed.connect(_handle_ws_state_changed)
 
-# func _handle_ws_state_changed(connection_state: WSClient.WSClientState) -> void:
-#   if connection_state is WSClient.AuthenticatedState:
-#     MultiplayerClient.start()
+  add_child(state)
+  state.add_child(roaming_state)
+  state.add_child(game_state)
+  state.change_state(roaming_state)
 
-func emote_triggered(chatter: Chatter, emote: String):
-  var bot := get_or_create_bot_for_user(chatter)
-  bot.emote = emote
-
-func chatter_updated(updated: Chatter):
-  var bot := get_or_create_bot_for_user(updated)
-  bot.chatter = updated
-
-func store_data_received():
-  for chatter in WSClient.authenticated_state.active_chatters:
-    get_or_create_bot_for_user(chatter)
-
-const gumbot_template: PackedScene = preload("res://stream_overlay/stream_overlay_gumbot.tscn")
-const DEFAULT_BOT_SCALE = Vector3(0.6, 0.6, 0.6)
-func get_or_create_bot_for_user(chatter: Chatter) -> GumBot:
-  if spawned_bots.has(chatter.id):
-    return spawned_bots[chatter.id]
-  else:
-    var bot: GumBot = gumbot_template.instantiate()
-    spawned_bots[chatter.id] = bot
-
-    add_child(bot)
-    bot.chatter = chatter
-
-    bot.global_position = bot_spawn_location.global_position + Vector3(randf_range(-1.5, 1.5), 0, randf_range(-.5, .5))
-    return bot
-
-func bot_expired(bot: GumBot):
-  bot.queue_free()
-  spawned_bots.erase(bot.chatter.id)
-
-func chat_message_received(message: Message.Chat):
-  var bot := get_or_create_bot_for_user(message.chatter)
-  bot.chatter = message.chatter
-
-  print("Chat message received: ", message.message, " from ", message.chatter.display_name)
+func _handle_ws_state_changed(connection_state: WSClient.WSClientState) -> void:
+  if connection_state is WSClient.AuthenticatedState:
+    MultiplayerClient.start()
 
 func _input(_event):
   if Input.is_action_just_pressed("StartLobby"):
@@ -70,22 +30,36 @@ func _input(_event):
     pass
 
 var pong_game_template: PackedScene = preload("res://games/pong/pong_game.tscn")
-var game_scene: PongGame = null
 func _handle_lobby_updated(lobby: Lobby) -> void:
-  print("Lobby updated: ", lobby)
   if lobby and lobby.started:
-    bot_walking_scene.visible = false
-    game_scene = pong_game_template.instantiate()
-    game_scene.lobby = lobby
-    print(lobby)
-    game_root_node.add_child(game_scene)
+    game_state.lobby = lobby
+    state.change_state(game_state)
 
-#   if Input.is_action_just_pressed("tts_skip"):
-#     tts_service.stop()
+class StreamOverlayState extends State:
+  var overlay: OverlayScene
+  func _init(_overlay: OverlayScene) -> void:
+    self.overlay = _overlay
 
-#   if Input.is_action_just_pressed("obs_scene_1"):
-#     WSClient.toggle_obs_scene("StreamBasic")
+class RoamingState extends StreamOverlayState:
+  var scene: RoamingBots
+  var roaming_bots_template: PackedScene = preload("res://stream_overlay/roaming_bots.tscn")
 
-#   if Input.is_action_just_pressed("debug_spawn_player"):
-#     # chat_message_received(Message.Chat.FromData({ mes}.FromData({ id="test", display_name="test", login="test", color="red", emote="" }))
-#     debug_bot_index += 1
+  func enter_state(_previous_state: State) -> void:
+    scene = roaming_bots_template.instantiate() as RoamingBots
+    overlay.add_child(scene)
+  
+  func exit_state() -> void:
+    scene.queue_free()
+
+class GameState extends StreamOverlayState:
+  var scene: PongGame
+  var lobby: Lobby
+  var pong_game_template: PackedScene = preload("res://games/pong/pong_game.tscn")
+
+  func enter_state(_previous_state: State) -> void:
+    scene = pong_game_template.instantiate() as PongGame
+    scene.lobby = lobby
+    overlay.add_child(scene)
+  
+  func exit_state() -> void:
+    scene.queue_free()
