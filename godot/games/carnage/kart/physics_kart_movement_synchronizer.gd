9db2e4ee8c4d85bb146bc53f2da14c0e38476b54
Joseph Sample <joseph.michael.sample@gmail.com>
2026-05-16 00:54:22 -0700

    wip physics

--- godot/games/carnage/kart/physics_kart_movement_synchronizer.gd
+++ godot/games/carnage/kart/physics_kart_movement_synchronizer.gd
@@ -12,8 +12,8 @@ extends MovementSynchronizer
 @export var min_speed_threshold: float = 0.01
 @export var mass: float = 5.0
 @export var gravity: float = 2.0
-@export var angular_damp: float = 0.8
-@export var surface_friction: float = 4.0
+@export var angular_damp: float = 0.2
+@export var surface_friction: float = 1.0
 @export var collision_torque_scale: float = 0.03
 @export var flip_velocity_threshold: float = 0.2
 @export var flip_up_dot_threshold: float = 0.5
@@ -39,7 +39,7 @@ var _visual_rest_local_basis := Basis.IDENTITY
 var _interp_visual_tween: Tween = null
 
 const MAX_WHEEL_ANGLE := deg_to_rad(35.0)
-const WHEEL_TURN_SPEED := deg_to_rad(120.0)
+const WHEEL_TURN_SPEED := deg_to_rad(80.0)
 
 func _ready() -> void:
   super._ready()
